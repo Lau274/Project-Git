f2d9235f1144f32d991c5524cffa3706b29f702d
Mario Tonnarelli <tonnarellilaurent@gmail.com>
2026-08-27 19:52:04 +0200

    aulab-oggetti-2

--- selfwork-variabili/script.js
+++ selfwork-variabili/script.js
@@ -1,10 +1,67 @@
-let a = 5;
-let b = 2;
-console.log(a,b);
-let str = "ciao";
-console.log(str);
-str = "1234";
-console.log(str);
-const PIGRECO = 3.14159265359;
-console.log(PIGRECO);
-PIGRECO = 0;
+let agenda = {
+
+    contacts: [
+        { nome: "Nicola", telefono: "3331111111" },
+        { nome: "Lorenzo", telefono: "3332222222" },
+        { nome: "Paola", telefono: "3333333333" },
+        { nome: "Jenny", telefono: "3334444444" }
+    ],
+
+    mostraTutti() {
+
+        for (let contatto of this.contacts) {
+            console.log(contatto.nome, contatto.telefono)
+        }
+
+    },
+
+    mostraContatto(nome) {
+
+        for (let contatto of this.contacts) {
+
+            if (contatto.nome === nome) {
+                console.log(contatto.nome, contatto.telefono)
+                return
+            }
+
+        }
+        console.log("Contatto non trovato")
+    },
+
+    eliminaContatto(nome) {
+
+        for (let i = 0; i < this.contacts.length; i++) {
+
+            if (this.contacts[i].nome === nome) {
+                this.contacts.splice(i, 1)
+                return
+            }
+
+        }
+
+        console.log("Contatto non trovato")
+    },
+
+    aggiungiContatto(nome, telefono) {
+
+        let nuovoContatto = {
+            nome: nome,
+            telefono: telefono
+        }
+
+        this.contacts.push(nuovoContatto)
+
+    },
+
+    modificaContatto(nome, nuovoTelefono) {
+
+        for (let contatto of this.contacts) {
+
+            if (contatto.nome === nome) {
+                contatto.telefono = nuovoTelefono
+                return
+            }
+        }
+        console.log("Contatto non trovato")
+    }
+}
