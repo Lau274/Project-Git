fbf4f2b4ab1e81ba1505409d916fec31e0699ff1
Mario Tonnarelli <tonnarellilaurent@gmail.com>
2026-08-27 21:23:47 +0200

    aulab-selfwork-oggetti-3

--- selfwork-aulab/script.js
+++ selfwork-aulab/script.js
@@ -1,67 +1,71 @@
-let agenda = {
-
-    contacts: [
-        { nome: "Nicola", telefono: "3331111111" },
-        { nome: "Lorenzo", telefono: "3332222222" },
-        { nome: "Paola", telefono: "3333333333" },
-        { nome: "Jenny", telefono: "3334444444" }
+let bowling = {
+    giocatori : [
+        {'name': 'Livio', 'scores': []},
+        {'name': 'Paola', 'scores': []},
+        {'name': 'Filippo', 'scores': []},
+        {'name': 'Giuseppe', 'scores': []}
     ],
+    punteggi() {
+    for (let giocatore of this.giocatori){
+        for (let i=0;i<10;i++){
+        giocatore.scores.push(Math.floor(Math.random() * (10 - 1 +1) + 1))
+}
+        console.log(giocatore.scores)
+        }
+    },
+    punteggio_finale() {
+        
+    for (let giocatore of this.giocatori){
+        let finale = 0
+        for(let i = 0;i<giocatore.scores.length;i++){
+            finale += giocatore.scores[i]
+        }
+        giocatore.totale = finale
+        console.log(giocatore.totale)
+        }
+        
+    },
+        aggiungiGiocatore(nome) {
 
-    mostraTutti() {
-
-        for (let contatto of this.contacts) {
-            console.log(contatto.nome, contatto.telefono)
+        let nuovoGiocatore = {
+            name: nome,
+            scores: []
         }
 
+        for (let i = 0; i < 10; i++) {
+
+            let punteggio = Math.floor(Math.random() * 10) + 1
+            nuovoGiocatore.scores.push(punteggio)
+        }
+        this.giocatori.push(nuovoGiocatore)
     },
 
-    mostraContatto(nome) {
 
-        for (let contatto of this.contacts) {
+    vincitore() {
 
-            if (contatto.nome === nome) {
-                console.log(contatto.nome, contatto.telefono)
-                return
-            }
-
-        }
-        console.log("Contatto non trovato")
+        this.punteggio_finale()
+        this.giocatori.sort((a, b) => b.totale - a.totale)
+        console.log(
+            `Il vincitore è ${this.giocatori[0].name} con ${this.giocatori[0].totale} punti`
+        )
     },
 
-    eliminaContatto(nome) {
+    classifica() {
 
-        for (let i = 0; i < this.contacts.length; i++) {
+        this.punteggio_finale()
+        this.giocatori.sort((a, b) => b.totale - a.totale)
+        for (let i = 0; i < this.giocatori.length; i++) {
 
-            if (this.contacts[i].nome === nome) {
-                this.contacts.splice(i, 1)
-                return
-            }
-
+            console.log(
+                `${i + 1}° ${this.giocatori[i].name}: ${this.giocatori[i].totale} punti`
+            )
         }
-
-        console.log("Contatto non trovato")
-    },
-
-    aggiungiContatto(nome, telefono) {
-
-        let nuovoContatto = {
-            nome: nome,
-            telefono: telefono
-        }
-
-        this.contacts.push(nuovoContatto)
-
-    },
-
-    modificaContatto(nome, nuovoTelefono) {
-
-        for (let contatto of this.contacts) {
-
-            if (contatto.nome === nome) {
-                contatto.telefono = nuovoTelefono
-                return
-            }
-        }
-        console.log("Contatto non trovato")
     }
 }
+
+
+bowling.punteggi()
+bowling.punteggio_finale()
+bowling.aggiungiGiocatore("Mario")
+bowling.classifica()
+bowling.vincitore()
